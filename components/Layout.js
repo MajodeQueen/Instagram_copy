73c443416bf6de240cf77164dadfc7fd3b92d874
Nakyonyimaj <majorinekayondo@gmail.com>
2023-03-25 21:46:59 +0300

    ft- added header and footer on small screens

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import React, { useContext, useEffect, useReducer, useState } from 'react';
 import TabComponent from './verticalNavComps/tabzComponent';
 import Search from './verticalNavComps/tabzComponent/search';
-import { BsInstagram } from 'react-icons/bs';
+import { BsHeart, BsInstagram, BsPerson, BsSearch } from 'react-icons/bs';
 import Notifications from './verticalNavComps/tabzComponent/notifications';
 import More from './verticalNavComps/tabzComponent/more';
 import CreateWrapper from './createWrapper';
@@ -12,7 +12,9 @@ import FollowComp from './followComp';
 import axios from 'axios';
 import PostDetails from './postDetails';
 import { Store } from '@/utils/Store';
-
+import { MdOutlinePhotoCamera } from 'react-icons/md';
+import { GrHomeRounded } from 'react-icons/gr';
+import { CgAddR } from 'react-icons/cg';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -27,7 +29,6 @@ const reducer = (state, action) => {
   }
 };
 
-
 export default function Layout({ children, title }) {
   const [open, setOpen] = useState(true);
   const [notOpen, setNotOpen] = useState(false);
@@ -38,8 +39,8 @@ export default function Layout({ children, title }) {
     error: false,
   });
 
-  const {state:cxtState} = useContext(Store);
-  const {openPostDetails} = cxtState;
+  const { state: cxtState } = useContext(Store);
+  const { openPostDetails } = cxtState;
 
   const fetchAll = async () => {
     try {
@@ -64,9 +65,6 @@ export default function Layout({ children, title }) {
     fetchAll();
   }, []);
 
-
-  
-  
   return (
     <>
       <Head>
@@ -92,6 +90,26 @@ export default function Layout({ children, title }) {
         pauseOnHover
       />
 
+      <header>
+        <div className="md:hidden  flex items-center w-full h-14 justify-between fixed z-10  bg-white shadow-lg">
+          <div className="px-2">
+            <img
+              src="/images/PngItem_1327993.png"
+              alt="insta img"
+              width="100"
+            />
+          </div>
+          <div className="flex items-center space-x-4 px-2">
+            <MdOutlinePhotoCamera className="ml-2 text-3xl" />
+            <BsHeart className="text-2xl" />
+            <img
+              src="/images/pngkit_send-icon-png_1882778.png"
+              alt=""
+              className="w-6 h-6"
+            />
+          </div>
+        </div>
+      </header>
 
       <div
         className={`flex flex-col justify-between md:grid md:grid-cols-5 md:gap-16   min-h-screen md:flex-row bg-white relative `}
@@ -102,22 +120,21 @@ export default function Layout({ children, title }) {
           } ease-in duration-300 border-r-[1px]
           `}
         >
-          
           <div
             className={`md:block hidden pt-5  px-2 fixed  
               }`}
           >
             <div className="pb-4 px-4">
-            {!open || notOpen ? (
-              <BsInstagram className="ml-2 text-2xl" />
-            ) : (
-              <img
-                src="/images/PngItem_1327993.png"
-                alt="insta img"
-                width="100"
-              />
-            )}
-          </div>
+              {!open || notOpen ? (
+                <BsInstagram className="ml-2 text-2xl" />
+              ) : (
+                <img
+                  src="/images/PngItem_1327993.png"
+                  alt="insta img"
+                  width="100"
+                />
+              )}
+            </div>
             <TabComponent
               open={open}
               setOpen={setOpen}
@@ -127,62 +144,88 @@ export default function Layout({ children, title }) {
               setMore={setMore}
               create={create}
               setCreate={setCreate}
-              userInfo={userInfo} 
+              userInfo={userInfo}
             />
           </div>
         </div>
         <div
           className={`min-h-screen  ease-in-out duration-300 fixed left-16 ${
             open ? 'translate-x-0 ' : 'translate-x-4 shadow-md rounded bg-white'
-          } ${!notOpen ? 'translate-x-0' : 'translate-x-4 shadow-md rounded bg-white'}`}
+          } ${
+            !notOpen
+              ? 'translate-x-0'
+              : 'translate-x-4 shadow-md rounded bg-white'
+          }`}
         >
           {!open && !notOpen && <Search />}
           {notOpen && open && <Notifications />}
         </div>
 
-        {more? (
+        {more ? (
           <div className="fixed left-4 w-[16%]  bottom-5 shadow rounded-md">
-            <More/>
+            <More />
           </div>
-        ):""}
+        ) : (
+          ''
+        )}
 
         <div className="absolute z-50">
-          {create && 
-          (
+          {create && (
             <div>
-              <CreateWrapper create={create} setCreate={setCreate}  userInfo={ userInfo} />
+              <CreateWrapper
+                create={create}
+                setCreate={setCreate}
+                userInfo={userInfo}
+              />
             </div>
           )}
         </div>
 
-
         <div className="absolute z-50">
-          {openPostDetails && 
-          (
+          {openPostDetails && (
             <div>
-              <PostDetails/>
+              <PostDetails />
             </div>
           )}
         </div>
 
         <div className="md:col-span-4">
           <main>
-            <div className={`grid grid-cols-5 min-h-screen`}>
-              <div className={`${openPostDetails?' overflow-hidden ':''}col-span-5 md:col-span-3 mt-8 `}>
+            <div className={`grid grid-cols-5 min-h-screen mt-6 md:mt-0`}>
+              <div
+                className={`${
+                  openPostDetails ? ' overflow-hidden ' : ''
+                }col-span-5 md:col-span-3 mt-8 `}
+              >
                 {children}
               </div>
-              <div className="col-span-2 hidden md:block mt-8 ">
-                <FollowComp userInfo={userInfo} error={error} followusers={followusers}/>
+              <div className="col-span-2 hidden md:block mt-8  z-10">
+                <FollowComp
+                  userInfo={userInfo}
+                  error={error}
+                  followusers={followusers}
+                />
               </div>
             </div>
           </main>
         </div>
-        <div className="block md:hidden">
-          <footer>footer</footer>
+        <div className="block md:hidden fixed bottom-0 shadow-lg mt-2 w-full">
+          <footer>
+            <div className="flex items-center h-16 justify-around bg-white">
+              <GrHomeRounded  className='text-2xl'/>
+              <BsSearch className='text-2xl' />
+              <CgAddR  className='text-2xl'/>
+              <img
+                src="/images/instagram-reels-13408 (1).png"
+                alt="reel img"
+                width="22"
+              />
+
+              <BsPerson  className='text-2xl'/>
+            </div>
+          </footer>
         </div>
       </div>
     </>
   );
 }
-
-
